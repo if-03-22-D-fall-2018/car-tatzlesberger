b6708439d9c2867841cb66b700e5fa2a5e6552be
tatzlesberger <t.atzlesberger@gmx.at>
2018-12-06 09:16:20 +0100

    First Commit

---
+++ car.cpp
@@ -0,0 +1,56 @@
+#include "car.h"
+
+struct CarImplementation
+{
+  enum CarType car_type;
+  enum Color color;
+  bool is_availale;
+  int fill_level;
+  double acceleration_rate;
+  int speed;
+};
+
+Car get_car(enum CarType)
+{
+
+}
+
+enum CarType get_type(Car car)
+{
+
+}
+
+enum Color get_color(Car car)
+{
+
+}
+
+int get_fill_level(Car car)
+{
+
+}
+
+double get_acceleration_rate(Car car)
+{
+
+}
+
+int get_speed(Car car)
+{
+
+}
+
+void set_acceleration_rate(Car car,int rate)
+{
+
+}
+
+void accelerate(Car car)
+{
+
+}
+
+void init()
+{
+
+}
--- car.h
+++ car.h
@@ -13,4 +13,19 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+enum CarType {AIXAM, JEEP, FIAT_MULTIPLA};
+enum Color {RED, GREEN, BLUE, ORANGE, SILVER, BLACK};
+
+typedef struct CarImplementation* Car;
+
+Car get_car(enum CarType);
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
+int get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+int get_speed(Car car);
+void set_acceleration_rate(Car car,int rate);
+void accelerate(Car car);
+void init();
+
 #endif
